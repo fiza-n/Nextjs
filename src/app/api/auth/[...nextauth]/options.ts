b127d8d09e2d1c984f80f2f863ba8d5ed0ce0bb7
fiza noor <fnoor034726@gmail.com>
2026-03-26 00:45:41 +0500

    using credentials provider in nextauth

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -10,13 +10,13 @@ export const authOptions : NextAuthOptions = {
             id:  "credentials",
             name: "Credentials",
             credentials: {
-            email: { label: "Email", type: "type" },
+            email: { label: "Email", type: "text" },
             password: {label: "Password", type: "password"}
             },
             async authorize(credentials: any):Promise<any> {
                 await dbConnect() 
                 try {
-                    const user = UserModel.findOne({
+                    const user = await UserModel.findOne({
 
                         $or : [
                             {email: credentials.identifier},
@@ -27,10 +27,29 @@ export const authOptions : NextAuthOptions = {
                     if(!user){
                         throw new Error("No user found with the provided email or username")
                     }
-                } catch (error: any) {
-                    
+                    if(!user.isVerified){
+                         throw new Error("Please verify your account before login")
+                    }
+                   const isPasswordCorrect =  await bcrypt.compare(credentials.password , user.password)
+
+                   if(isPasswordCorrect) {
+                    return user
+                   }
+                   else{
+                    throw new Error("Incorrect password!")
+                   }
+                }
+                 catch (error: any) {
+                    throw new Error(error)
                 }
             }
          })
-    ]
+    ],
+    pages : {
+        signIn: '/sign-in'
+    },
+    session : {
+        strategy :  "jwt"
+    },
+    secret : process.env.NEXTAUTH_SECRET
 }
